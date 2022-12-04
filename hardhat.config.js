a5451470136078c5eeb4a6b9d31aeb8aaec0d23b
Sascha <sascha.fh.kubisch@gmail.com>
2022-12-04 17:50:18 +0100

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("@nomicfoundation/hardhat-toolbox");
+const config = {
+    solidity: "0.8.17",
+    paths: { tests: "tests" },
+};
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners();
+    for (const account of accounts) {
+        console.log(account.address);
+    }
+});
+exports.default = config;
